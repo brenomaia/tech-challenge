3e8464e42287931635d5b9d52fdaf6a586ec55b7
Breno Maia <iosdevbreno@gmail.com>
2019-04-11 00:14:31 -0300

    Implement transfer with split and auxiliary function
    Auxiliary function to check if the Float Shares sum 1.0

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -22,6 +22,32 @@ defmodule FinancialSystem do
     end
   end
 
+  @spec transfer_to_many(Account.t(), [Account.t()], Integer.t(), [Float.t()]) :: {:ok, Account.t(), [Account.t()]} | {:error, String}
+  def transfer_to_many(origin_acc, destination_accs, amount, share_percentage) when is_list(destination_accs) do 
+
+    unless has_same_currency?([origin_acc] ++ destination_accs) do
+      {:error, "Your destination account currency must match your source account"}
+    end
+
+    unless has_enough_balance?(origin_acc, amount) do
+      raise(ArgumentError, message: "You don't have enough money on your origin account")
+    end
+
+    unless check_shares_percentage(destination_accs, share_percentage) do
+      raise(ArgumentError, message: "You need to have as many shares as destination accounts and they have to sum 1.0")
+    end
+
+    {:ok, Account.withdraw(origin_acc, Money.new(amount, origin_acc.balance.currency)), Enum.map(Enum.with_index(destination_accs), fn({r, s}) -> 
+      Account.deposit(r, Money.new(round(amount * Enum.at(share_percentage, s)), r.balance.currency))
+    end)}
+
+  end
+
+  @spec check_shares_percentage([Account.t()], [Float.t()]) :: boolean
+  def check_shares_percentage(accounts, shares) do
+    Enum.count(accounts) == Enum.count(shares) && Enum.sum(shares) == 1.0
+  end
+
   @spec has_same_currency?(List.t()) :: boolean
   def has_same_currency?(accounts) do 
     currency = List.first(accounts).balance.currency
