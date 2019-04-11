bc49cf811e8c4d56b14670de59640aff71f6a027
Breno Maia <iosdevbreno@gmail.com>
2019-04-11 00:10:51 -0300

    Implement transfer to another account func and auxiliary functions
    Auxiliary functions to check if the accounts have the same currency and if the origin account has enough money to transfer

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -6,6 +6,34 @@ defmodule FinancialSystem do
   alias Money.Currency, as: Currency
   alias FinancialSystem.Account, as: Account
 
+  @spec transfer_to_one(Account.t(), Account.t(), Integer.t()) ::  {:ok, Account.t(), Account.t()} | {:error, String.t()}
+  def transfer_to_one(origin_acc, destination_acc, amount) do
+
+    unless has_same_currency?([origin_acc, destination_acc]) do
+      {:error, "Your destination account currency must match your source account"}
+    end
+
+    if has_enough_balance?(origin_acc, amount) do
+      {:ok, Account.withdraw(origin_acc, Money.new(amount, origin_acc.balance.currency)), 
+        Account.deposit(destination_acc, Money.new(amount, origin_acc.balance.currency))}
+        
+    else 
+      raise(ArgumentError, message: "You don't have enough money on your origin account")
+    end
+  end
+
+  @spec has_same_currency?(List.t()) :: boolean
+  def has_same_currency?(accounts) do 
+    currency = List.first(accounts).balance.currency
+    Enum.all?(accounts, fn(s) -> s.balance.currency == currency end)
+  end
+
+  @spec has_enough_balance?(Account.t(), Integer.t()):: boolean 
+  def has_enough_balance?(acc, amount) do 
+    # Money.compare(acc.balance, Money.new(amount, acc.balance.currency)) == 1
+    acc.balance.amount >= amount
+  end
+
   @spec convert_account(Account.t(), Currency.t(), Float.t()) :: Account.t() | {:error, String.t()}
   def convert_account(account, currency, rate) do
     
